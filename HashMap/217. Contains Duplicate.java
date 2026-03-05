9ff4d486ce26f40d6a76b137141c78f813d21e1c
n hemanth kuamr <hemanthtech143@gmail.com>
2026-03-05 21:11:36 +0530

    new files

---
+++ HashMap/217. Contains Duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+          HashMap<Integer,Integer> map = new HashMap<>();
+    for (int x : nums){
+
+
+        if (map.containsKey(x)) {
+            return true;
+        }
+        map.put(x, 1);
+    }
+
+
+    return false;
+    }
+}
---
+++ HashMap/242. Valid Anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+         HashMap<Character, Integer> map = new HashMap<>();
+    HashMap<Character, Integer> map1 = new HashMap<>();
+
+    for (int i = 0; i < s.length() ; i++) {
+        char c = s.charAt(i);
+        map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+   for (int i = 0; i < t.length(); i++) {
+        char c = t.charAt(i);
+        map.put(c, map.getOrDefault(c, 0) - 1);
+    }
+
+    for (int val : map.values()) {
+        if (val != 0) return false;
+    }
+    return true;
+    }
+}
